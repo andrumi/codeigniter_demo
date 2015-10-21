91aab85de936153bb46cdc2c54e8ec7913547292
1228264 <a.gilson@wlv.ac.uk>
2015-10-21 16:59:45 +0100

    CodeIgnitor framework added

---
+++ application/views/pages/crap.php
@@ -0,0 +1,11 @@
+<?$this->load->helper('url');?>
+<html>
+<head>
+   <title>crap</title>
+   
+</head>
+<body>
+   <h1>CodeIgniter 2 Tutorial</h1>
+<p>
+<img src="<?=base_url("images/DodgyDan.jpg")?>">
+</p>  
