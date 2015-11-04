2bd2c6780942b68d37992d8a114c0547211c66f6
1228264 <a.gilson@wlv.ac.uk>
2015-11-04 17:49:38 +0000

    Added the bootstrap layout to the header and rearranged the other files so that header, footer and page all are using the bootstrap layout.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
+<html lang="en">
 <html>
      <head>
-       <title>CodeIgniter Tutorial: this is the template header</title>
+       <title>CodeIgniter Tutorial:</title>
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
       
     </head>
         <body>
         <div id = "header">
                 <h1><?php echo $title.": HEADER here"; ?></h1>
         </div>
-        <div id= "navBar">
-        </div>
+        
