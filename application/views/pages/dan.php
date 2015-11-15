da31e436ba328a201c51612fc9ffdeb4053a3b01
1228264 <a.gilson@wlv.ac.uk>
2015-11-15 22:07:54 +0000

    Experimenting with image size, using em instead of px for size measurements

--- application/views/pages/dan.php
+++ application/views/pages/dan.php
@@ -1,11 +1,11 @@
-
 <?$this->load->helper('url');?>
- <link rel="stylesheet" type="text/css" href="<?=base_url("css/style2.css")?>">
+ <link rel="stylesheet" type="text/css" href="<?=base_url("css/New2.css")?>">
 <div id = "content">
    <h1>CodeIgniter 2 Tutorial</h1>
 <p>
 
-<img src="<?=base_url("images/DodgyDan.jpg")?>">
+<img src="<?=base_url("images/DodgyDan.jpg")?>" width = "460" height = "345">
+
 </p>  
 </div>
 
