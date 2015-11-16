1a2524b1c83b4999d2d1d58f04abe7a3ecfa7d49
1228264 <a.gilson@wlv.ac.uk>
2015-11-16 21:46:46 +0000

    Links added to navbar, logon dropdown form-no php.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -21,20 +21,25 @@
              <div>
                  <ul class="nav navbar-nav">
                      <li class="active"><a href="<?php echo site_url('news/'); ?>">Home</a></li>
-                     <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
-                         <ul class="dropdown-menu">
-                             <li><a href="dan.php">Page 1-1</a></li>
-                             <li><a href="#">Page 1-2</a></li>
-                             <li><a href="#">Page 1-3</a></li>
-                         </ul>
-                     </li>
-                     <li><a href="#">Page 2</a></li>
+                     <li><a href="<?php echo site_url('news/create'); ?>">Create</a></li>
                      <li><a href="#">Page 3</a></li>
                  </ul>
                  <ul class="nav navbar-nav navbar-right">
-                     <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                     <li><a href="<?php echo site_url('news/register'); ?>"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
 <!--                     <li><a href="../../pages/view/logon"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>-->
                      <li><a href="<?php echo site_url('pages/view/logon/'); ?>"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                     <li class="dropdown">
+                         <a class="dropdown-toggle" href="#" data-toggle="dropdown"><span class="glyphicon glyphicon-log-in"></span> Login<strong class="caret"></strong></a>
+                         <div class="dropdown-menu" style="padding: 15px; padding-bottom: 5px;">
+                             <form method="post" action="login" accept-charset="UTF-8">
+                                 <input style="margin-bottom: 15px;" type="text" placeholder="Username" id="username" name="username">
+                                 <input style="margin-bottom: 15px;" type="password" placeholder="Password" id="password" name="password">
+                                 <input style="float: left; margin-right: 10px;" type="checkbox" name="remember-me" id="remember-me" value="1">
+                                 <label class="string optional" for="user_remember_me"> Remember me</label>
+                                 <input class="btn btn-primary btn-block" type="submit" id="sign-in" value="Sign In">
+                             </form>
+                         </div>
+                     </li>
 
                  </ul>
              </div>
