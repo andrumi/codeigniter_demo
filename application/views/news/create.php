b7d12a8f86f57fb5a6400c4937c834d1fc890bb1
1228264 <a.gilson@wlv.ac.uk>
2015-11-16 21:50:10 +0000

    Create form rearranged to use header and footer

--- application/views/news/create.php
+++ application/views/news/create.php
@@ -1,15 +1,30 @@
-<h2><?php echo $title; ?></h2>
+<div class="jumbotron">
+    <h1><?php echo $title; ?></h1>
+</div>
+<div class="row">
+    <div class="col-sm-4">
+        <h2><?php echo $title; ?></h2>
+        <?php echo validation_errors(); ?>
+        <?php echo form_open('news/create'); ?>
 
-<?php echo validation_errors(); ?>
+        <label for="title">Title</label>
+        <input type="input" name="title" /><br />
 
-<?php echo form_open('news/create'); ?>
+        <label for="text">Text</label>
+        <textarea name="text"></textarea><br />
 
-    <label for="title">Title</label>
-    <input type="input" name="title" /><br />
+        <input type="submit" name="submit" value="Create news item" />
 
-    <label for="text">Text</label>
-    <textarea name="text"></textarea><br />
+        </form>
+    </div>
+    <div class="col-sm-4">
+        <h3>Column 2</h3>
+    </div>
+    <div class="col-sm-4">
+        <h3>Column 3</h3>
+        <p>words go here</p>
+    </div>
 
-    <input type="submit" name="submit" value="Create news item" />
+</div>
 
-</form>
+
