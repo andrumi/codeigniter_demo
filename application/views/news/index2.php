29655358cfc903f87608f7ce7fd70ac3b6ff7515
1228264 <a.gilson@wlv.ac.uk>
2015-11-16 21:52:04 +0000

    Added create news form. This form should possibly be put at the top and the news output in reverse order - newest first

--- application/views/news/index2.php
+++ application/views/news/index2.php
@@ -2,31 +2,49 @@
     <h1><?php echo $title; ?></h1>
 </div>
 <div class="row">
-<!--    <div class="col-sm-4">-->
-<!--        <h3>Column 1</h3>-->
-<!--        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>-->
-<!--        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>-->
-<!--    </div>-->
-<!--    <div class="col-sm-4">-->
-<!--        <h3>Column 2</h3>-->
-<!--        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>-->
-<!--        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>-->
-<!--    </div>-->
-<!--    <div class="col-sm-4">-->
-<!--        <h3>Column 3</h3>-->
-<!--        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>-->
-<!--        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>-->
-<!--    </div>-->
-<h2><?php echo $title; ?></h2>
 
-<?php foreach ($news as $news_item): ?>
+    <div class="col-sm-4">
+        <h3>Column 1</h3>
+        <h3><?php echo $title; ?></h3>
 
-        <h3><?php echo $news_item['title']; ?></h3>
-        <div class="main">
+        <?php foreach ($news as $news_item): ?>
+
+            <h3><?php echo $news_item['title']; ?></h3>
+            <div class="main">
                 <?php echo $news_item['text']; ?>
-        </div>
-        <p><a href="<?php echo site_url('news/view/'.$news_item['slug']); ?>">View article</a></p>
+            </div>
+            <p><a href="<?php echo site_url('news/view/'.$news_item['slug']); ?>">View article</a></p>
 
-<?php endforeach; ?>
+        <?php endforeach; ?>
+
+    </div>
+    <div class="col-sm-4">
+        <h3>Column 2</h3>
+        <p>words go here</p>
+    </div>
 </div>
 
+<div class="row">
+
+    <div class="col-sm-4">
+        <h3>Column3</h3>
+        <h2><?php echo $title; ?></h2>
+        <?php echo validation_errors(); ?>
+        <?php echo form_open('news/create'); ?>
+
+        <label for="title">Title</label>
+        <input type="input" name="title" /><br />
+
+        <label for="text">Text</label>
+        <textarea name="text"></textarea><br />
+
+        <input type="submit" name="submit" value="Create news item" />
+
+        </form>
+
+    </div>
+    <div class="col-sm-4">
+        <h3>Column 4</h3>
+        <p>words go here</p>
+    </div>
+</div>
