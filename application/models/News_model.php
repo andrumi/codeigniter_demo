8b14bf166696392032617d467a9af5d4048caeee
1228264 <a.gilson@wlv.ac.uk>
2015-11-17 10:54:19 +0000

    index() now refreshes the index page when news items have been submitted

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -9,13 +9,23 @@ class News_model extends CI_Model {
 		public function get_news($slug = FALSE)
         {
           if ($slug === FALSE)
-             {
+            {
+                $this->db->order_by("date", "desc");
                 $query = $this->db->get('news');
                 return $query->result_array();
              }
 
-          $query = $this->db->get_where('news', array('slug' => $slug));
-          return $query->row_array();
+            $query = $this->db->get_where('news', array('slug' => $slug));//,'order_by(date)DESC'
+            return $query->row_array();
+
+
+
+        }
+        public function get_new_news()
+        {
+            $this->db->order_by("date", "asc");
+            $query = $this->db->get($this->table_name);
+            return $query->result();
         }
 		
 		public function set_news()
