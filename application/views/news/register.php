a33642de9c799ac2f2b8cc5bd5036ddefeab75b7
1228264 <a.gilson@wlv.ac.uk>
2015-11-18 00:20:05 +0000

    The form for registration has been added. Hopefully the form for uploading a profile picture can also be included.

--- application/views/news/register.php
+++ application/views/news/register.php
@@ -46,13 +46,31 @@
             <legend>Upload Photo</legend>
             <ol>
                 <li>
-                    <label for=address>Address</label>
+                    <label for=address>Upload</label>
                     <textarea id=address name=address rows=5 required></textarea>
                 </li>
-<!--                <li>-->
-<!--                    <label for=postcode>Post code</label>-->
-<!--                    <input id=postcode name=postcode type=text required>-->
-<!--                </li>-->
+                <li>
+                    <input type="file" id="files" name="files[]" multiple />
+                    <output id="list"></output>
+
+<!--                    <script>-->
+<!--                        function handleFileSelect(evt) {-->
+<!--                            var files = evt.target.files; // FileList object-->
+<!---->
+<!--                            // files is a FileList of File objects. List some properties.-->
+<!--                            var output = [];-->
+<!--                            for (var i = 0, f; f = files[i]; i++) {-->
+<!--                                output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',-->
+<!--                                    f.size, ' bytes, last modified: ',-->
+<!--                                    f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',-->
+<!--                                    '</li>');-->
+<!--                            }-->
+<!--                            document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';-->
+<!--                        }-->
+<!---->
+<!--                        document.getElementById('files').addEventListener('change', handleFileSelect, false);-->
+<!--                    </script>-->
+                </li>
 <!--                <li>-->
 <!--                    <label for=country>Country</label>-->
 <!--                    <input id=country name=country type=text required>-->
