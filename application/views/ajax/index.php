b789112a6dc9ecdf454b07497c580fbf8fa57aa2
1228264 <a.gilson@wlv.ac.uk>
2015-11-19 13:19:47 +0000

    Now outputs the results from the search. The results are also links to the corresponding news items

--- application/views/ajax/index.php
+++ application/views/ajax/index.php
@@ -1,3 +1,18 @@
  <!-- application/views/ajax/index.php-->
 
-<p><?=$ajaxdata?></p>
+  <p>
+      <?
+      echo $ajaxdata1.'<br>';
+      $arrlength = count($ajaxdata);
+
+      if ($arrlength > 0) {
+          foreach ($ajaxdata as $ajaxdata_item) {
+
+      ?>
+        <a href="<?php echo site_url('news/view/'.$ajaxdata_item['slug']); ?>"> <?echo $ajaxdata_item['title'].'<br>';?> </a>
+      <?
+          }
+      }
+      else echo "No matches found.."
+     ?>
+  </p>
