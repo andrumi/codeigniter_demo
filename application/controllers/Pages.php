be7d7421c7530e86f4afb5aa58906068a95cc181
1228264 <a.gilson@wlv.ac.uk>
2015-11-19 16:10:42 +0000

    Updated for use with the registration form.

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,6 +1,13 @@
 <?php
 class Pages extends CI_Controller {
 
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->model('pages_model');
+
+        }
+
         public function view($page = 'home')
         {
 		   
@@ -15,6 +22,36 @@ class Pages extends CI_Controller {
         $this->load->view('templates/header', $data);
         $this->load->view('pages/'.$page, $data);
         $this->load->view('templates/footer', $data);
+        }
+
+        public function index()
+        {
+            $this->load->helper('form');
+            $this->load->library('form_validation');
+
+            $data['title'] = 'Register';
+
+            $this->form_validation->set_rules('username', 'Username', 'required');
+//            $this->form_validation->set_rules('email', 'Email', 'required');
+//            $this->form_validation->set_rules('town', 'Town', 'required');
+//            $this->form_validation->set_rules('password', 'Password', 'required');
+
+            if ($this->form_validation->run() === FALSE)
+            {
+                $this->load->view('templates/header', $data);
+                $this->load->view('pages/index');
+                $this->load->view('templates/footer');
+
+            }
+            else
+            {
+                $this->pages_model->register();
+
+                $this->load->view('templates/header', $data);
+                $this->load->view('pages/index');
+                $this->load->view('templates/footer');
+
+            }
         }
 }
 ?>
