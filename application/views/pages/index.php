03f508c2f89cec1d099b52ee5456450f7306c019
1228264 <a.gilson@wlv.ac.uk>
2015-11-19 16:12:24 +0000

    Registration form allows new members to be added to database. The file upload has now stopped working as there are two forms.

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -4,42 +4,43 @@
     <div class="col-sm-4">
       <h3>Register</h3>
        <h2>Log On</h2>
-        <form role="form">
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter email">
-            </div>
-            <div class="form-group">
-                <label for="pwd">Password:</label>
-                <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-            </div>
-            <div class="checkbox">
-                <label><input type="checkbox"> Remember me</label>
-            </div>
-            <button type="submit" class="btn btn-default">Submit</button>
-          </form>
+<!--        <form role="form">-->
+<!--            <div class="form-group">-->
+<!--                <label for="email">Email:</label>-->
+<!--                <input type="email" class="form-control" id="email" placeholder="Enter email">-->
+<!--            </div>-->
+<!--            <div class="form-group">-->
+<!--                <label for="pwd">Password:</label>-->
+<!--                <input type="password" class="form-control" id="pwd" placeholder="Enter password">-->
+<!--            </div>-->
+<!--            <div class="checkbox">-->
+<!--                <label><input type="checkbox"> Remember me</label>-->
+<!--            </div>-->
+<!--            <button type="submit" class="btn btn-default">Submit</button>-->
+<!--          </form>-->
         </div>
     <div class="col-sm-4">
-
+<!--        **********NONSENSE WITH FORMS***************-->
+    <form id="Form1" action="pages/index" method="post"></form>
     <form id="upload" action="upload" method="POST" enctype="multipart/form-data">
         <fieldset>
             <legend>Your details</legend>
             <ol>
                 <li>
                     <label for=username>Username</label>
-                    <input id=username name=username type=text placeholder="username" required autofocus>
+                    <input id=username name=username type=text placeholder="username" required autofocus form="Form1"  >
                 </li>
                 <li>
                     <label for=email>Email</label>
-                    <input id=email name=email type=email placeholder="example@domain.com" required>
+                    <input id=email name=email type=email placeholder="example@domain.com" form="Form1" required >
                 </li>
                 <li>
                     <label for=town>Town</label>
-                    <input id=town name=town type=text placeholder="town" required>
+                    <input id=town name=town type=text placeholder="town" required form="Form1" >
                 </li>
                 <li>
                     <label for=password>Password</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" required>
+                    <input type="password" class="form-control" name="password" placeholder="Enter password" required form="Form1" >
                 </li>
             </ol>
         </fieldset>
@@ -52,7 +53,7 @@
 
                         <div>
                             <label for="fileselect">Files to upload:</label>
-                            <input type="file" id="fileselect" name="fileselect[]" multiple="multiple" />
+                            <input type="file" id="fileselect" name="fileselect[]" multiple="multiple" form="upload"/>
                             <div id="filedrag">or drop files here</div>
                         </div>
                         <div id="submitbutton">
@@ -67,11 +68,26 @@
             </ol>
         </fieldset>
         <fieldset>
-            <button type=submit>Register</button>
+<!--            <button type=submit>Register</button>-->
+            <input type="submit" name="button1" value="Register" form="Form1" />
+
         </fieldset>
     </form>
-
+    </form>
     </div>
 
     </div>
     <script type="text/javascript" src="<?=base_url("js/filedrag.js")?>" ></script>
+
+<!-- Nested forms?
+<form id="Form1" action="Action1.php" method="post"></form>
+<form id="Form2" action="Action2.php" method="post"></form>
+
+<input type="text" name="input_Form1_n1" form="Form1" />
+<input type="text" name="input_Form2_n1" form="Form2" />
+<input type="text" name="input_Form1_n2" form="Form1" />
+<input type="text" name="input_Form2_n2" form="Form2" />
+
+<input type="submit" name="button1" value="buttonVal1" form="Form1" />
+<input type="submit" name="button2" value="buttonVal2" form="Form2" />
+-->
