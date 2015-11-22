b6fb98e9ce6c9a2c1fc6905fda3c701a325d3e78
1228264 <a.gilson@wlv.ac.uk>
2015-11-22 21:04:34 +0000

    The controller for the chatter live chat. Opens views/chatter/index.php

---
+++ application/controllers/Chatter.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrew
+ * Date: 22/11/2015
+ * Time: 10:28
+ */
+
+
+// application/controller/ajax.php
+
+class Chatter extends CI_Controller
+{
+    public function index()
+    {
+        $data['title'] = 'Chatter';
+        $this->load->view('templates/header',$data);
+        $this->load->view('chatter/index');
+        $this->load->view('templates/footer');
+    }
+    public function push_event()
+    {
+        $data['title'] = 'Chatter';
+        $this->load->view('push-event');
+
+    }
+}
