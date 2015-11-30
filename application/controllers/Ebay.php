4b942b9e7c1294e0f4dd022ae42cafd5ac797c33
1228264 <a.gilson@wlv.ac.uk>
2015-11-30 21:59:35 +0000

    The page returns a search from the ebay API. This could be tidied up or put in a panel, but it does work.

---
+++ application/controllers/Ebay.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: andrew
+ * Date: 30/11/2015
+ * Time: 20:03
+ */
+class Ebay  extends CI_Controller {
+
+//    public function __construct()
+//    {
+//        parent::__construct();
+//        $this->load->model('ajax_model');
+//
+//    }
+
+
+    public function index()
+    {
+
+        $data['title'] = 'Ebay Search Results';
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('ebay/index', $data);
+        $this->load->view('templates/footer');
+
+    }
+
+}
---
+++ application/views/ebay/index.php
@@ -0,0 +1,37 @@
+<!--<style type="text/css">body { font-family: arial,sans-serif;} </style>-->
+<?php
+//if(isset($scripts))
+//{
+//    foreach($scripts as $script) {
+//        echo $script;
+//    }
+//}?>
+<script type="text/javascript" src="<?=base_url("js/ebay.js")?>" ></script>
+<!--<script>-->
+<!--    s=document.createElement('script'); // create script element-->
+<!--    s.src= url;-->
+<!--    document.body.appendChild(s);-->
+<!--</script>-->
+<div class="jumbotron">
+    <h1><?php echo $title; ?></h1>
+
+</div>
+<div class="row">
+    <div class="col-sm-4">
+        <h3>Results</h3>
+
+        <div id="results"></div>
+
+    </div>
+    <div class="col-sm-4">
+        <h3>Column 2</h3>
+
+    </div>
+    <div class="col-sm-4">
+        <h3>Column 3</h3>
+
+    </div>
+</div>
+
+
+<!--</div>-->
