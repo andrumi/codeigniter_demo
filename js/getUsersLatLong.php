e2098ca2eb8fe58afcf28e45e9e09c959f6c3c10
1228264 <a.gilson@wlv.ac.uk>
2015-12-06 10:45:56 +0000

    Get username and longtitude and latitude from database

---
+++ js/getUsersLatLong.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrew
+ * Date: 05/12/2015
+ * Time: 20:29
+ */
+
+$server = "localhost";
+$database="db1228264";
+$username = "1228264";
+$password = "Bernard1";
+
+// Create connection
+$conn = mysqli_connect($server, $username, $password,$database);
+if ($conn->connect_error) {
+    print_r("inside conn failed");
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$query=("SELECT username, latitude,longtitude FROM members where longtitude != 'null' ");
+$to_encode = array();
+
+if ($result=mysqli_query($conn,$query)){
+   while ($row=mysqli_fetch_row($result))
+    {
+        $to_encode[] = $row;
+    }
+}
+else{
+    print_r("inside php result failure!");
+}
+
+echo json_encode($to_encode);
