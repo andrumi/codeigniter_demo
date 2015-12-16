526484302ba2cb5e29b467cf2136548814aa5ac8
1228264 <a.gilson@wlv.ac.uk>
2015-12-16 13:26:19 +0000

    Passing a variable for username to chatter. This is in preparation for passing session user name.

--- application/controllers/Chatter.php
+++ application/controllers/Chatter.php
@@ -14,6 +14,7 @@ class Chatter extends CI_Controller
     public function index()
     {
         $data['title'] = 'Chatter';
+        $data['user']= 'sessionUserGoesHere';
         $this->load->view('templates/header',$data);
         $this->load->view('chatter/index');
         $this->load->view('templates/footer');
